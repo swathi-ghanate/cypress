2f135647a0c94ef9cff843ed66eba2cd071046fa
swathi-ghanate <swathi.g@beehyv.com>
2023-05-01 17:56:01 +0530

    change

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
 const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-    specPattern: 'cypress/integration/examples/*.js'
-  },
+     },
+    specPattern: 'cypress/integration/examples/*.js',
+ //cypress.json 
+
+  chromeWebSecurity: false
+
+   
+ },
 });
--- cypress/integration/examples/Test5.js
+++ cypress/integration/examples/Test5.js
@@ -2,15 +2,17 @@
 Handling child tab with combination  of cypress&Jquery:Cypress can't handle when it opens in seperate tab/window 
 If we open in same tab/window it can handle it.Cypress have ability to mainpulate the DOM-removeAttr() for this we need
 to use jquery method 
-Get the url and Nagivate back using browsers control
+Get the url and Nagivate back using browsers control. //to remove attribute we are using Jquery. jquery it as method removeAttr().to invoke jquery function we need to use .invoke
+//to get url we have url() commond and include is used to validate substring in assertion
+//to navigate we can use go
+
 */
-describe('My fifth Test Suite', function() 
-{
- 
-it('My Fifth Test case',function() {
- 
-//Check boxes
+describe('My fifth Test Suite', () => {
+
+it('My Fifth Test case', () => {
+    
 cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+//Check boxes
 cy.get('#alertbtn').click()
 cy.get('[value="Confirm"]').click()
 //window:alert
@@ -25,12 +27,8 @@ cy.on('window:confirm',(str)=>
     //Mocha
     expect(str).to.equal('Hello , Are you sure you want to confirm?')
 })
- //to remove attribute we are using Jquery. jquery it as method removeAttr().to invoke jquery function we need to use .invoke
-cy.get('#opentab').invoke('removeAttr','target').click()
-//to get url we have url() commond and include is used to validate substring in assertion
-cy.url().should('eq', '')
-
-//to navigate we can use go
-cy.go('back')
+cy.get('#opentab').invoke('removeAttr','target').click();
+cy.wait(15000)
+cy.url().should('include','qaclickacademy')
 }  )
 }  )
