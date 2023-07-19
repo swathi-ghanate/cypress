5d6d046d97693a677bb91b806acdeef6c1a0d5d6
swathi-ghanate <swathi.g@beehyv.com>
2023-07-19 15:55:15 +0530

    indentation

--- cypress/integration/examples/API_Assignments/GetProduct.js
+++ cypress/integration/examples/API_Assignments/GetProduct.js
@@ -9,36 +9,29 @@ verify id is 25
 verify brand: ""Dry Rose""
 verify category: ""groceries""
 verify price: 70"   */
-/// <reference types="Cypress" />
+< reference types = "Cypress" / >
 
+describe('Get Product by ID', () => {
+   it('should retrieve product information with ID 25', () => {
+      const productId = 25;
+      const endpoint = `https://dummyjson.com/products/${productId}`;
 
-/* describe('template spec', () => {
-    it('passes', () => {
-        //Getting response from browserstack demo website 
-    cy.request('GET','https://dummyjson.com/products/25').then((response) => */
-    
-    describe('Get Product by ID', () => {
-        it('should retrieve product information with ID 25', () => {
-          const productId = 25;
-          const endpoint = `https://dummyjson.com/products/${productId}`;
-      
-          cy.request({
-            method: 'GET',
-            url: endpoint,
-          }).then((response)=>
-     {
-    //Expecting the response status code to be 200
-    expect(response.status).to.eq(200)
-     //Expecting the response status text to be OK
-     expect(response.statusText).to.eq("OK")
-     //Expecting the response id is 25
-     expect(response.body.id).to.eq(25)
- //Expecting the response brand: ""Dry Rose""
- expect(response.body.brand).to.eq("Dry Rose")
-  //Expecting the response category: ""groceries""
-  expect(response.body.category).to.eq("groceries")
-    //Expecting the response  price: 70" 
-    expect(response.body.price).to.eq(70)
-    })
-    })
-    })
+      cy.request({
+         method: 'GET',
+         url: endpoint,
+      }).then((response) => {
+         //Expecting the response status code to be 200
+         expect(response.status).to.eq(200)
+         //Expecting the response status text to be OK
+         expect(response.statusText).to.eq("OK")
+         //Expecting the response id is 25
+         expect(response.body.id).to.eq(25)
+         //Expecting the response brand: ""Dry Rose""
+         expect(response.body.brand).to.eq("Dry Rose")
+         //Expecting the response category: ""groceries""
+         expect(response.body.category).to.eq("groceries")
+         //Expecting the response  price: 70" 
+         expect(response.body.price).to.eq(70)
+      })
+   })
+})
--- cypress/integration/examples/API_Assignments/GetProductQueryParam.js
+++ cypress/integration/examples/API_Assignments/GetProductQueryParam.js
@@ -8,36 +8,27 @@ verify number of products is 1.
 verify discountPercentage: 12.96
 verify stock: 94"  */
 
+ 
 describe('Get Product by Query Param', () => {
-    it('should retrieve product information using query param', () => {
-    
+   it('should retrieve product information using query param', () => {
       const endpoint = 'https://dummyjson.com/products/search';
-  
       cy.request({
-        method: 'GET',
-        url: endpoint, qs:{q :'Mobile'}
-      }).then((response)=> {
-        // Verify status code
-        expect(response.status).to.eq(200);
-  
-        // Verify status text
-        expect(response.statusText).to.eq('OK');
-  
-        // Verify number of products
-        expect(response.body.products[0].id).to.eq(1)
-  
-        // Verify discountPercentage
-        expect(response.body.products[0].discountPercentage).to.eq(12.96);
-  
-        // Verify stock
-        expect(response.body.products[0].stock).to.eq(94);
+         method: 'GET',
+         url: endpoint,
+         qs: {
+            q: 'Mobile'
+         }
+      }).then((response) => {
+         // Verify status code
+         expect(response.status).to.eq(200);
+         // Verify status text
+         expect(response.statusText).to.eq('OK');
+         // Verify number of products
+         expect(response.body.products[0].id).to.eq(1)
+         // Verify discountPercentage
+         expect(response.body.products[0].discountPercentage).to.eq(12.96);
+         // Verify stock
+         expect(response.body.products[0].stock).to.eq(94);
       });
-    });
-  });
-  
-  
-  
-  
-  
-  
-  
+   });
+});
